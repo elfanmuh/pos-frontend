37fd0b9b4ce5ed4a6a8d27d041f9c6588006dfe3
elfanmuh <elfanmuh@gmail.com>
2025-11-02 10:52:12 +0700

    update app.js and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const endpoint = "http://localhost:3000/api/v1/items"
-  const [items, setItems] = useState([])
-
-  const fetchItems = async () => {
-    const response = await fetch(endpoint)
-    const result = await response.json()
-    console.log(result)
-    setItems(result)
-  }
-
-  useEffect(() => {
-    fetchItems()
-  }, [])
 
   return (
     <>
       <h1>TESTING</h1>
-      {items.data?.map((item) => {
-        return (
-          <div key={item.id}>
-            <p>{item.name}</p>
-            <p>{item.price}</p>
-          </div>
-        )
-      })}
+      <div>
+        <button>Test</button>
+      </div>
     </>
   )
 }
