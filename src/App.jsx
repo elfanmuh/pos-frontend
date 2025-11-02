248a16e5a42d44127be372c7356fd830849b787f
Muhammad Elfansyah <elfanmuh@gmail.com>
2025-11-02 11:27:34 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,10 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  let [count, setCount] = useState(0)
 
-  const add = () => {
-    setCount(count + 1)
-  }
-
-  const min = () => {
-    setCount(count - 1)
-  }
   return (
     <>
       <h1>TESTING</h1>
-      <div>
-        <button onClick={add}>+</button>
-        <button onClick={min}>-</button>
-      </div>
-      <div>
-
-        <h2>{count}</h2>
-      </div>
     </>
   )
 }
